24531eef2c78f977c8439f05ff2658947c6a5b1a
Meyu-Sys <sarveshkate0@gmail.com>
2023-01-09 18:36:04 +0530

    add

--- debsetup.sh
+++ debsetup.sh
@@ -11,6 +11,8 @@ fi
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 cp sources.list /etc/apt/sources.list
 
+username=$(id -u -n 1000)
+
 # Updating Packages
 apt update
 apt upgrade
@@ -28,4 +30,42 @@ curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debi
 nala update
 nala install fish
 
-#
+# Creating dirs and moving configs
+
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/.fonts
+mkdir -p /home/$username/Pictures
+mkdir -p /usr/share/sddm/themes
+cp -R dotconfig/* /home/$username/.config/
+chown -R $username:$username /home/$username
+tar -xzvf sugar-candy.tar.gz -C /usr/share/sddm/themes
+mv /home/$username/.config/sddm.conf /etc/sddm.conf
+
+# Installing sugar-candy dependencies
+nala install libqt5svg5 qml-module-qtquick-controls qml-module-qtquick-controls2 -y
+
+# Installing Packages
+
+nala install sddm dolphin wget unzip zip 
+
+# Installing fonts 
+
+nala install fonts-font-awesome
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d /home/$username/.fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
+unzip Meslo.zip -d /home/$username/.fonts
+mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
+chown $username:$username /home/$username/.fonts/*
+
+# Reloading Font
+fc-cache -vf
+# Removing zip Files
+rm ./FiraCode.zip ./Meslo.zip
+
+# Install brave-browser
+sudo nala install apt-transport-https curl -y
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo nala update
+sudo nala install brave-browser -y
