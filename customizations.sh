e4a4c33d729b521d9a4cbcdd903df4cb82546ffd
Meyu-Sys <sarveshkate0@gmail.com>
2023-01-18 17:55:31 +0530

    Update customizations.sh

--- customizations.sh
+++ customizations.sh
@@ -11,43 +11,43 @@ dnf copr enable dturner/TOS
 dnf install obmenu-generator
 
 # Fonts
-mkdir -pv ~/.fonts/{Cantarell,Comfortaa,IcoMoon-Custom,Nerd-Patched,Unifont}
+mkdir -pv /home/$username/.fonts/{Cantarell,Comfortaa,IcoMoon-Custom,Nerd-Patched,Unifont}
 wget --no-hsts -cN https://download-fallback.gnome.org/sources/cantarell-fonts/0.303/cantarell-fonts-0.303.1.tar.xz
-wget --no-hsts -cNP ~/.fonts/Unifont/ https://unifoundry.com/pub/unifont/unifont-14.0.02/font-builds/unifont-14.0.02.ttf
-wget --no-hsts -cNP ~/.fonts/Nerd-Patched/ https://github.com/owl4ce/dotfiles/releases/download/ng/{M+.1mn,Iosevka}.Nerd.Font.Complete.Mono.ttf
-wget --no-hsts -cNP ~/.fonts/Nerd-Patched/ https://github.com/owl4ce/dotfiles/releases/download/ng/M+.1mn.Nerd.Font.Complete.mono.ttf
-wget --no-hsts -cNP ~/.fonts/IcoMoon-Custom/ https://github.com/owl4ce/dotfiles/releases/download/ng/{Feather,Material}.ttf
-wget --no-hsts -cNP ~/.fonts/Comfortaa/ https://raw.githubusercontent.com/googlefonts/comfortaa/main/fonts/OTF/Comfortaa-{Bold,Regular}.otf
-tar -xvf cantarell*.tar.xz --strip-components 2 --wildcards -C ~/.fonts/Cantarell/ \*/\*/Cantarell-VF.otf
+wget --no-hsts -cNP /home/$username/.fonts/Unifont/ https://unifoundry.com/pub/unifont/unifont-14.0.02/font-builds/unifont-14.0.02.ttf
+wget --no-hsts -cNP /home/$username/.fonts/Nerd-Patched/ https://github.com/owl4ce/dotfiles/releases/download/ng/{M+.1mn,Iosevka}.Nerd.Font.Complete.Mono.ttf
+wget --no-hsts -cNP /home/$username/.fonts/Nerd-Patched/ https://github.com/owl4ce/dotfiles/releases/download/ng/M+.1mn.Nerd.Font.Complete.mono.ttf
+wget --no-hsts -cNP /home/$username/.fonts/IcoMoon-Custom/ https://github.com/owl4ce/dotfiles/releases/download/ng/{Feather,Material}.ttf
+wget --no-hsts -cNP /home/$username/.fonts/Comfortaa/ https://raw.githubusercontent.com/googlefonts/comfortaa/main/fonts/OTF/Comfortaa-{Bold,Regular}.otf
+tar -xvf cantarell*.tar.xz --strip-components 2 --wildcards -C /home/$username/.fonts/Cantarell/ \*/\*/Cantarell-VF.otf
 
 dnf install fonts-font-awesome fonts-noto
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
-unzip FiraCode.zip -d ~/.fonts
+unzip FiraCode.zip -d /home/$username/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Meslo.zip
-unzip Meslo.zip -d ~/.fonts
-mv dotfonts/fontawesome/otfs/*.otf ~/.fonts/
-chown $username:$username ~/.fonts/*
+unzip Meslo.zip -d /home/$username/.fonts
+mv dotfonts/fontawesome/otfs/*.otf /home/$username/.fonts/
+chown $username:$username /home/$username/.fonts/*
 # Removing zip Files
 rm ./FiraCode.zip ./Meslo.zip
 
 # Icons
-mkdir -pv ~/.icons
+mkdir -pv /home/$username/.icons
 wget --no-hsts -cN https://github.com/owl4ce/dotfiles/releases/download/ng/{Gladient_JfD,Papirus{,-Dark}-Custom}.tar.xz
-tar -xf Gladient_JfD.tar.xz -C ~/.icons/
-tar -xf Papirus-Custom.tar.xz -C ~/.icons/
-tar -xf Papirus-Dark-Custom.tar.xz -C ~/.icons/
-sudo ln -vs ~/.icons/Papirus-Custom /usr/share/icons/
-sudo ln -vs ~/.icons/Papirus-Dark-Custom /usr/share/icons/
+tar -xf Gladient_JfD.tar.xz -C /home/$username/.icons/
+tar -xf Papirus-Custom.tar.xz -C /home/$username/.icons/
+tar -xf Papirus-Dark-Custom.tar.xz -C /home/$username/.icons/
+sudo ln -vs /home/$username/.icons/Papirus-Custom /usr/share/icons/
+sudo ln -vs /home/$username/.icons/Papirus-Dark-Custom /usr/share/icons/
 rm {Gladient_JfD.tar.xz,Papirus-Custom.tar.xz,Papirus-Dark-Custom.tar.xz}
 
 # wallpaper
-mkdir -pv ~/.wallpapers/{mechanical,eyecandy}
-wget -P ~/.wallpaper/mechanical/ https://github.com/owl4ce/depfiles/raw/3.2/.wallpaper/mechanical/mechanical-key_2K.jpg
+mkdir -pv /home/$username/.wallpapers/{mechanical,eyecandy}
+wget -P /home/$username/.wallpaper/mechanical/ https://github.com/owl4ce/depfiles/raw/3.2/.wallpaper/mechanical/mechanical-key_2K.jpg
 
 # Extensions
-mkdir -pv ~/.urxvt/ext
-(cd ~/.urxvt/ext/; curl -LO https://raw.githubusercontent.com/simmel/urxvt-resize-font/master/resize-font)
-(cd ~/.urxvt/ext/; curl -LO https://raw.githubusercontent.com/mina86/urxvt-tabbedex/master/tabbedex)
+mkdir -pv /home/$username/.urxvt/ext
+(cd /home/$username/.urxvt/ext/; curl -LO https://raw.githubusercontent.com/simmel/urxvt-resize-font/master/resize-font)
+(cd /home/$username/.urxvt/ext/; curl -LO https://raw.githubusercontent.com/mina86/urxvt-tabbedex/master/tabbedex)
 
 # FOnt cache
 fc-cache -vf
